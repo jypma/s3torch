56cc1e6d74a8f4247d0eba9269a1c62f4bbd0ef1
Jan Ypma <jan@ypmania.nl>
2025-12-13 10:32:25 +0100

    Add "Ref" so we can define unknown dimensions

--- src/main/scala/net/ypmania/s3torch/Dim.scala
+++ src/main/scala/net/ypmania/s3torch/Dim.scala
@@ -22,6 +22,10 @@ object Dim extends DimLowPriorityGivens {
     override def size = _size
   }
 
+  case class Ref[D <: Dim](ref: D) extends Dim {
+    override def size = ref.size
+  }
+
   // The "+ 0L" hack here is needed, since scala 3.7.4 otherwise will allow Long variables to match here, even though
   // their compile-time value is unknown.
   given fromLongStatic[L <: Long & Singleton](using ValueOf[L], ValueOf[L + 0L]): Conversion[L, Static[L]] with {
@@ -33,12 +37,17 @@ object Dim extends DimLowPriorityGivens {
   /** A dimension known to be 1 at compile time */
   type One = Static[1L]
 
-  type Max[D1 <: Dim, D2 <: Dim] <: Dim = (D1, D2) match {
-    case (Static[s1], Static[s2]) => (s1 > s2) match {
-      case true => D1
-      case false => D2
+  type Max[D1 <: Dim, D2 <: Dim] <: Dim = D1 match {
+    case D2 => D1 // Same type => pick any
+    case _ => (D1, D2) match {
+      case (One, _) => D2 // Either Dim is one => pick the other
+      case (_, One) => D1
+      case (Static[s1], Static[s2]) => (s1 > s2) match {
+        case true => D1 // If static, pick whichever is bigger
+        case false => D2
+      }
+      case _ => Dynamic // Fallback, we don't know statically which one is bigger
     }
-    case _ => Dynamic
   }
 
 }
