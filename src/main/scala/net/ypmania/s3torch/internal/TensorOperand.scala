adcbe8a3ba0318d228b449805967f4ee6d76656d
Jan Ypma <jan@ypmania.nl>
2025-12-14 11:49:03 +0100

    Rename to proper name

--- src/main/scala/net/ypmania/s3torch/internal/TensorOperand.scala
+++ src/main/scala/net/ypmania/s3torch/internal/TensorOperand.scala
@@ -5,6 +5,7 @@ import net.ypmania.s3torch.Tensor
 import net.ypmania.s3torch.Shape
 import net.ypmania.s3torch.DType
 import net.ypmania.s3torch.Promoted
+import net.ypmania.s3torch.Dim
 
 /** Type class that exists for V where V can be the operand to an operation on a tensor with either a scalar (resulting in the same shaped tensor), or another tensor (resulting in Broadcast being applied) */
 trait TensorOperand[V] {
@@ -22,4 +23,5 @@ object TensorOperand {
     type Out[S <: Tuple, T <: DType] = Tensor[Broadcast[S, S2], Promoted[T, T2]]
     override def apply[S <: Tuple, T <: DType](t: Tensor[S, T], v: Tensor[S2, T2], withScalar: (pytorch.Tensor, pytorch.Scalar) => pytorch.Tensor, withTensor: (pytorch.Tensor, pytorch.Tensor) => pytorch.Tensor) = new Tensor(withTensor(t.native, v.native))
   }
+
 }
